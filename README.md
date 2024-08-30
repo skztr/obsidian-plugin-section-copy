ee4aa4ec28b0e94dd42802313cfc9cb6365ab7d7
skztr <skizotoro@gmail.com>
2024-08-30 21:58:09 +0100

    base - initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Obsidian Base Plugin
+
+This plugin exists for the purpose of being a bare-bones starting point
+for the creation of a new obsidian plugin.
---
+++ esbuild.config.mjs
@@ -0,0 +1,39 @@
+import esbuild from "esbuild";
+import process from "process";
+import builtins from "builtin-modules";
+
+const watch = process.argv.includes("--watch");
+
+const context = await esbuild.context({
+	entryPoints: ["main.ts"],
+	bundle: true,
+	external: [
+		"obsidian",
+		"electron",
+		"@codemirror/autocomplete",
+		"@codemirror/collab",
+		"@codemirror/commands",
+		"@codemirror/language",
+		"@codemirror/lint",
+		"@codemirror/search",
+		"@codemirror/state",
+		"@codemirror/view",
+		"@lezer/common",
+		"@lezer/highlight",
+		"@lezer/lr",
+		...builtins],
+	format: "cjs",
+	target: "es2018",
+	logLevel: "info",
+	sourcemap: watch ? "inline" : false,
+	treeShaking: true,
+	outfile: "main.js",
+	minify: prod,
+});
+
+if (watch) {
+	await context.watch();
+} else {
+	await context.rebuild();
+	process.exit(0);
+}
